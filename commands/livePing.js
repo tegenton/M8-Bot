d898f17b9238c08de42d9b052e8d6cde8ef42ab2
Mitchell Reiff <me@mreiff.space>
2018-04-28 15:42:00 -0400

    Liveping

--- commands/livePing.js
+++ commands/livePing.js
@@ -1,38 +1,38 @@
 exports.run = (client, message) => {
-  const settings = client.settings.get(message.guild.id);
-  const args = message.content.split(" ").slice(1);
-  if (args[0] == null) {
-    var option = "bad"
-  } else {
-    var option = args[0].toString()
-  }
-  var serverSettings = client.settings.get(message.guild.id)
+  // const settings = client.settings.get(message.guild.id);
+  // const args = message.content.split(" ").slice(1);
+  // if (args[0] == null) {
+  //   var option = "bad"
+  // } else {
+  //   var option = args[0].toString()
+  // }
+  // var serverSettings = client.settings.get(message.guild.id)
 
-  if (option == "bad") {
-    message.reply(`the valid settings options are **on** and **off**.`)
-    return;
-  }
+  // if (option == "bad") {
+  //   message.reply(`the valid settings options are **on** and **off**.`)
+  //   return;
+  // }
 
-  if (option.toString().toLowerCase() == "off") {
-    serverSettings.livePing = false
-    client.settings.set(message.guild.id, serverSettings)
-    message.reply(`the live ping has been disabled.`)
-    return;
-  }
-  if (option.toString().toLowerCase() == "on") {
-    serverSettings.livePing = true
-    client.settings.set(message.guild.id, serverSettings)
-    message.reply(`the live ping has been enabled.`)
-    return;
-  } else {
-    message.reply(`${option} is not a valid setting. The valid settings options are **on** and **off**.`)
-    return;
-  }
+  // if (option.toString().toLowerCase() == "off") {
+  //   serverSettings.livePing = false
+  //   client.settings.set(message.guild.id, serverSettings)
+  //   message.reply(`the live ping has been disabled.`)
+  //   return;
+  // }
+  // if (option.toString().toLowerCase() == "on") {
+  //   serverSettings.livePing = true
+  //   client.settings.set(message.guild.id, serverSettings)
+  //   message.reply(`the live ping has been enabled.`)
+  //   return;
+  // } else {
+  //   message.reply(`${option} is not a valid setting. The valid settings options are **on** and **off**.`)
+  //   return;
+  // }
 }
 
 
 exports.conf = {
-  enabled: false,
+  enabled: true,
   guildOnly: true,
   aliases: [],
   permLevel: "Administrator"
