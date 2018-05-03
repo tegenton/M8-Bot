8dc97d37fa434182ccc9b72a283709ef65d85cd2
Mitchell Reiff <me@mreiff.space>
2018-05-03 16:25:51 -0400

    Update Snek

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -8,15 +8,27 @@ module.exports = async (client, guild) => {
   // Well they're gone. Let's remove them from the settings!
   await client.settings.get(guild.id).delete().run();
 
-  const fetch = require("snekfetch");
+  // const fetch = require("snekfetch");
 
-  // discordbots_org
-  new fetch("POST", `https://discordbots.org/api/bots/${client.user.id}/stats`)
-    .set("Authorization", settings.discordbots_org)
-    .send({
-      server_count: client.guilds.size
-    })
-    .then(() => console.log("Updated dbots.org status.")).catch((e) => e);
+  // // discordbots_org
+  // new fetch("POST", `https://discordbots.org/api/bots/${client.user.id}/stats`)
+  //   .set("Authorization", settings.discordbots_org)
+  //   .send({
+  //     server_count: client.guilds.size
+  //   })
+  //   .then(() => console.log("Updated dbots.org status.")).catch((e) => e);
+
+  const fetch = require('snekfetch')
+  const dbotstoken = settings.discordbots_org
+
+  client.shard.fetchClientValues('guilds.size').then(result => {
+    const guildsizes = result.reduce((prev, val) => prev + val, 0)
+    fetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
+      .set('Authorization', settings.discordbots_org)
+      .send({ "server_count": guildsizes })
+      .then(() => console.log(`dbotsstats updated successfully`))
+      .catch(err => console.error(err))
+  })
 
   // bots.discord.pw
   new fetch("POST", `https://bots.discord.pw/api/bots/${client.user.id}/stats`)
