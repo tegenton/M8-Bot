d4197d4b69f9a671f5043fab628dac45f7629ba6
Mitchell Reiff <me@mreiff.space>
2018-09-07 17:11:04 -0400

    v11.3.1
    removed double post fix to test of it fixes it

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var version = "11.3";
+var version = "11.3.1";
 module.exports.version = version;
 // This will check if the node version you are running is the required
 // Node version, if it isn't it will throw the following error to inform
@@ -209,15 +209,21 @@ function mixerCheck() {
             }
             var lastLiveTime = fs.readFileSync("./mixer_time/" + mixerInfo.token + "_time.txt", "utf-8"); //checks the last live time
             var timeDiff = liveTime - lastLiveTime; //gets the diff of current and last live times
-            if (timeDiff >= halfHour) { //if its been 30min or more
+
+
+            // if (timeDiff >= halfHour) { //if its been 30min or more
               console.log(chalk.cyan(mixerInfo.token + " went live, as its been more than 30min!")); //log that they went live
               const hook = new Discord.WebhookClient(settings.liveID, settings.hookToken); //sets info about a webhook
               hook.sendMessage(`${mixerInfo.token} went live on Mixer!`);
               client.liveMixer(mixerInfo.token)
-            }
-            if (timeDiff < halfHour) { //if its been less than 30min
-              console.log(mixerInfo.token + " attempted to go live, but its been under 30min!"); //log that its been under 30min
-            }
+            // }
+
+
+            // if (timeDiff < halfHour) { //if its been less than 30min
+              // console.log(mixerInfo.token + " attempted to go live, but its been under 30min!"); //log that its been under 30min
+            // }
+
+
             // delay(10).then(() => {
               fs.writeFile("./mixer_time/" + mixerInfo.token + "_time.txt", liveTime); //update last live time regardless if they went live or not
             // });
@@ -261,7 +267,7 @@ function twitchCheck() {
     }
     var lastLiveTime = fs.readFileSync("./twitch_time/" + streamersTwitch[tc] + "_time.txt", "utf-8");
     var timeDiff = liveTime - lastLiveTime;
-    if (timeDiff >= halfHour) { //if its been 30min or more
+    // if (timeDiff >= halfHour) { //if its been 30min or more
       var request = require("request"); //the var to request details on the streamer
       request("https://api.twitch.tv/kraken/streams/" + streamersTwitch[tc] + "?client_id=" + settings.twitch_id, function (error, response, body) {
         if (!error && response.statusCode == 200) { //if there is no error
@@ -287,7 +293,7 @@ function twitchCheck() {
           }
         }
       });
-    }
+    // }
   }
 }
 
